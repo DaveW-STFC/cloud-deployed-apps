0b30d5929cde14071f2705fcceba25ba20c42780
DaveW-STFC <david.white@stfc.ac.uk>
2024-09-19 14:55:37 +0100

    COrrected bash file errors, trimmed down some statements

--- scripts/pr_check_copy.sh
+++ scripts/pr_check_copy.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # Script to check if the changed files to promote are a direct copy of earlier environment
+
 devEnv="dev"
 stagingEnv="staging"
 
-compare_files() [
-    file1 = $1
-    file2 = $2
-    file3 = $3
+compare_files() {
+    file1=$1
+    file2=$2
+    file3=$3
 
     if [ "$#" -eq 2 ]; then
         if cmp -s "$file1" "$file2" ; then
@@ -21,14 +22,14 @@ compare_files() [
         else
             return 1
         fi
-]
+    fi
+}
 
 # If the file is related to dev env then we are not interested.
 # Will look to filter these out in the yaml file of the GitHub action.
-if [[ $s1 == *"dev/"* ]] ; then
-    return 1;
-elif [[ $s1 == *"staging/"* ]] ; then
-    compare_files "$s1" "${$s1/staging/"$devEnv"}"
-elif [[$s1 == *"prod/"* ]] ; then
-    compare_files "$s1" "${$s1/prod/"$stagingEnv"}" "${$s1/prod/"$devEnv"}"
+if [[ $1 == *"staging/"* ]] ; then
+    compare_files "$1" "${!$1/staging/"$devEnv"}"
+else
+    compare_files "$1" "${!$1/prod/"$stagingEnv"}" "${!$1/prod/"$devEnv"}"
+fi
     
