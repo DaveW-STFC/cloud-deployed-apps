60aebf8d18e797a01b94cd1a9d8f00eb98bae98e
DaveW-STFC <david.white@stfc.ac.uk>
2024-09-24 10:06:58 +0100

    changes to try to check file is called

--- scripts/pr_check_copy.sh
+++ scripts/pr_check_copy.sh
@@ -2,36 +2,37 @@
 
 # Script to check if the changed files to promote are a direct copy of earlier environment
 
-devEnv="dev"
-stagingEnv="staging"
+# devEnv="dev"
+# stagingEnv="staging"
 
-compare_files() {
-    file1=$1
-    file2=$2
-    file3=$3
+# compare_files() {
+#     file1=$1
+#     file2=$2
+#     file3=$3
 
-    if [ "$#" -eq 2 ]; then
-        if cmp -s "$file1" "$file2" ; then
-            return 0
-        else
-            return 1
-        fi
-    else
-        if cmp -s "$file1" "$file2" && cmp -s "$file1" "$file3"; then
-            return 0
-        else
-            return 1
-        fi
-    fi
-}
+#     if [ "$#" -eq 2 ]; then
+#         if cmp -s "$file1" "$file2" ; then
+#             return 0
+#         else
+#             return 1
+#         fi
+#     else
+#         if cmp -s "$file1" "$file2" && cmp -s "$file1" "$file3"; then
+#             return 0
+#         else
+#             return 1
+#         fi
+#     fi
+# }
 
 # If the file is related to dev env then we are not interested.
 # Will look to filter these out in the yaml file of the GitHub action.
 echo "Running comparison"
 path=$1
-if [[ $1 == *"staging"* ]] ; then
-    compare_files "$1" "${path/staging/"$devEnv"}"
-else
-    compare_files "$1" "${path/prod/"$stagingEnv"}" "${path/prod/"$devEnv"}"
-fi
+return $path
+# if [[ $1 == *"staging"* ]] ; then
+#     compare_files "$1" "${path/staging/"$devEnv"}"
+# else
+#     compare_files "$1" "${path/prod/"$stagingEnv"}" "${path/prod/"$devEnv"}"
+# fi
     
