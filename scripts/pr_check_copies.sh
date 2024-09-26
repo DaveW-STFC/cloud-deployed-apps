db1533a0383b7fd99433cb81875547dcecc3dfa6
DaveW-STFC <david.white@stfc.ac.uk>
2024-09-26 11:38:10 +0100

    removed process echo statements as they were all being returned from script

--- scripts/pr_check_copies.sh
+++ scripts/pr_check_copies.sh
@@ -27,9 +27,8 @@ compare_files() {
 
 # If the file is related to dev env then we are not interested.
 # Will look to filter these out in the yaml file of the GitHub action.
-echo "Running comparison"
+
 path=$1
-echo "$path"
 if [[ $1 == *"staging"* ]] ; then
     compare_files "$1" "${path/staging/"$devEnv"}"
 else
